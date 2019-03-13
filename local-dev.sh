02969b71c22a13dce38ee5011054ba01d7e53db7
Arthur Ragimov <arthur.ragimov@gmail.com>
2019-03-13 16:57:18 +0200

    update

--- local-dev.sh
+++ local-dev.sh
@@ -2,7 +2,7 @@
 
 die() { echo "$*" 1>&2 ; exit 1; }
 
-echo 'Running strikepro-adminpanel local development container.'
+echo 'Running strikepro-admin-panel-frontend local development container.'
 
 read -p 'Type host port (default: 4002): ' HOST_PORT
 read -p 'Type host project path (required): ' HOST_PATH
@@ -20,5 +20,5 @@ CONTAINER_PORT=4000
 CONTAINER_PATH='/home/node/app'
 DOCKER_IMAGE_TAG=${DOCKER_IMAGE_TAG:-latest}
 
-echo "Run strikepro/adminpanel-frontend:${DOCKER_IMAGE_TAG}..."
-docker run -it -u node -p $HOST_PORT:4000 --name strikepro_adminpanel_frontend -v "$HOST_PATH":"$CONTAINER_PATH" --rm strikepro/adminpanel-frontend:$DOCKER_IMAGE_TAG
+echo "Run strikepro/admin-panel-frontend:${DOCKER_IMAGE_TAG}..."
+docker run -it -u node -p $HOST_PORT:4000 --name strikepro_adminpanel_frontend -v "$HOST_PATH":"$CONTAINER_PATH" --rm strikepro/admin-panel-frontend:$DOCKER_IMAGE_TAG
