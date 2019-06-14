9a19cce5ed4e5e78eb31a64ba606d7f63c47a340
fmakareev <fmakareev@code-artel.com>
2019-06-14 18:55:48 +0300

    Create URLAliasEditor

--- src/views/URLAlias/View/URLAliasEditor/index.js
+++ src/views/URLAlias/View/URLAliasEditor/index.js
@@ -1,14 +1,57 @@
 import React, {Component} from 'react';
-import PageContainer from "../../../Slides/View/SlidesEditor";
+import {connect as connectRestEasy} from "@brigad/redux-rest-easy";
+import {
+  GetUrlAliasAction,
+  GetUrlAliasById,
+  isRetrievingGetUrlAlias,
+  URL_ALIAS_RESOURCE_NAME
+} from "../../../../store/reduxRestEasy/UrlAlias/UrlAliasResource";
+import FormURLAliasEditor from "./Components/FormURLAliasEditor/FormURLAliasEditor";
+
 
 export class URLAliasEditor extends Component {
-  render() {
-    return (<PageContainer
-      header={'URL Alias editor'}
-    >
 
-    </PageContainer>)
+  componentWillMount() {
+    const {match: {params}, GetUrlAliasAction} = this.props;
+    if (params && params.id) {
+      GetUrlAliasAction({
+        urlParams: {
+          id: params.id,
+        }
+      });
+    }
+  }
+
+  render() {
+    const {urlAlias, match: {params}, isRetrievingGetUrlAlias} = this.props;
+
+    if (isRetrievingGetUrlAlias) {
+      return <div>Loading....</div>
+    }
+
+    let initialValues = {
+      initialValues: params && params.id && urlAlias(params.id)
+    };
+    return (
+      <FormURLAliasEditor {...initialValues}/>
+    )
   }
 }
+
+URLAliasEditor = connectRestEasy(
+  (state, ownProps) => ({
+    urlAlias: (id) => {
+      const data = GetUrlAliasById(state, URL_ALIAS_RESOURCE_NAME, id, ownProps);
+      if (data) {
+        return data[id];
+      }
+      return null;
+    },
+    isRetrievingGetUrlAlias: isRetrievingGetUrlAlias(state, ownProps),
+  }),
+  dispatch => ({
+    GetUrlAliasAction: (options) => dispatch(GetUrlAliasAction(options)),
+  })
+)(URLAliasEditor);
 
 export default URLAliasEditor;
