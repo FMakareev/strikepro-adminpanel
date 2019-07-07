e09d48f7734db6ac77b4103e065cb410a56bf53b
fmakareev <fmakareev@code-artel.com>
2019-07-07 21:18:18 +0300

    new build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.69ec680f3290f6d618e246cc45b3fdd6.js"
+  "/precache-manifest.9a7871c7be12ea80dd303dcd54062dd3.js"
 );
 
 self.addEventListener('message', (event) => {
